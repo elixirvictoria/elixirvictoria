8713d7c150a73ef3ae5aa441ad9f6ed3cb62ee5e
Alan Vardy <alanvardy@gmail.com>
2020-01-22 21:09:44 -0800

    disable unneeded user socket

--- lib/elixirvictoria_web/endpoint.ex
+++ lib/elixirvictoria_web/endpoint.ex
@@ -1,14 +1,15 @@
 defmodule ElixirVictoriaWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :elixirvictoria
 
-  socket "/socket", ElixirVictoriaWeb.UserSocket,
-    websocket: true,
-    longpoll: false
+  # socket "/socket", ElixirVictoriaWeb.UserSocket,
+  #   websocket: true,
+  #   longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
+
   plug Plug.Static,
     at: "/",
     from: :elixirvictoria,
