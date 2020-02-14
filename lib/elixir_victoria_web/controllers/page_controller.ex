783607276166a524f08488bab8cdf50c1cae132a
Alan Vardy <alanvardy@gmail.com>
2020-02-14 12:50:14 -0800

    Upcoming event

--- lib/elixir_victoria_web/controllers/page_controller.ex
+++ lib/elixir_victoria_web/controllers/page_controller.ex
@@ -1,6 +1,7 @@
 defmodule ElixirVictoriaWeb.PageController do
   use ElixirVictoriaWeb, :controller
+  alias ElixirVictoria.Group
 
   @spec index(Plug.Conn.t(), map) :: Plug.Conn.t()
-  def index(conn, _params), do: render(conn, "index.html")
+  def index(conn, _params), do: render(conn, "index.html", next_event: Group.next_event())
 end
