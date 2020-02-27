7c6a2880f268f76371883e2d64586e176f8a25e6
Alan Vardy <alanvardy@gmail.com>
2020-02-27 15:36:33 -0800

    Add tzdata and ex_dokku

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirVictoria.MixProject do
     [
       app: :elixir_victoria,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
@@ -53,6 +53,7 @@ defmodule ElixirVictoria.MixProject do
       {:ex_effective_bootstrap, "~> 0.1.15"},
       {:bamboo, "~> 1.4"},
       {:recaptcha, "~> 3.0"},
+      {:tzdata, "~> 1.0.3"},
       # Auth
       {:pow, "~> 1.0.17"},
       {:bodyguard, "~> 2.4"},
@@ -64,7 +65,9 @@ defmodule ElixirVictoria.MixProject do
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
       {:sobelow, "~> 0.8", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test, runtime: false},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:ex_dokku, "~> 0.1.7", only: :dev},
+      {:observer_cli, "~> 1.5"}
     ]
   end
 
