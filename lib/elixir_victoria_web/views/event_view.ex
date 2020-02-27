a90a43ccd6d5eca120542b36a91dee9fe2c64fef
Alan Vardy <alanvardy@gmail.com>
2020-02-27 15:41:48 -0800

    Add zulutime

--- lib/elixir_victoria_web/views/event_view.ex
+++ lib/elixir_victoria_web/views/event_view.ex
@@ -1,5 +1,7 @@
 defmodule ElixirVictoriaWeb.EventView do
   use ElixirVictoriaWeb, :view
+  alias ElixirVictoria.Group
+  alias ElixirVictoria.Group.Event
 
   @spec nice(Date.t()) :: binary
   def nice(%Date{} = date) do
@@ -42,5 +44,40 @@ defmodule ElixirVictoriaWeb.EventView do
       |> Kernel.-(1)
 
     Enum.at(@months, num)
+  end
+
+  @location "Tyee Housing Co-op Community Room, Unit 1 - 103 Wilson St, Victoria"
+
+  @doc "Makes an `Add to Google Calendar` button"
+  @spec add_to_google_calendar(Event.t()) :: {:safe, iolist}
+  def add_to_google_calendar(event) do
+    title = plusify("Elixir Victoria #{event.title}")
+    location = plusify(@location)
+    start_time = Group.to_zulu_time(event, :start)
+    end_time = Group.to_zulu_time(event, :end)
+
+    ~e"""
+    <a href="https://www.google.com/calendar/render?action=TEMPLATE
+    &text=<%= title %>
+    &dates=<%= start_time %>/<%= end_time %>
+    &details=For+details,+link+here:+https://www.elixirvictoria.com/events/<%= event.id %>
+    &location=<%= location %>
+    &sf=true&output=xml"
+    target="_blank"
+    class="btn btn-sm btn-primary"> <%= icon("calendar", class: "small-icon") %> Add to Google Calendar </a>
+    """
+  end
+
+  @spec plusify(binary) :: binary
+  defp plusify(sentence) do
+    sentence
+    |> String.replace(" ", "+")
+    |> String.replace("#", "")
+  end
+
+  @doc "Returns true if event is in the future"
+  @spec upcoming?(Event.t()) :: boolean
+  def upcoming?(event) do
+    Date.compare(Date.utc_today(), event.date) == :lt
   end
 end
